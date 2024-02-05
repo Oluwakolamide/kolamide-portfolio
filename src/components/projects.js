6ff3640d8f96bb0f9d0bfa0e42355f4dbe5439a8
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-02-06 00:21:39 +0100

    added view more button

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,18 @@
 import { projectlist } from "../static/projectlist";
 import { StyledProject } from "../styles/project.style";
-import Button from "./sections/button";
+// import Button from "./sections/button";
 import ProjectCard from "./sections/projectcard";
 import Title from "./sections/title";
 
 const Projects = () => {
   return (
-    <StyledProject>
+    <StyledProject id="projects">
       <Title sn={"03"} title={"Projects"} />
       <h2>Featured Projects:</h2>
       {projectlist.map((item, index) => (
         <ProjectCard key={index} item={item} />
       ))}
-      <Button text={"View All Projects"} />
+      <div className="more">View More Projets</div>
     </StyledProject>
   );
 };
--- src/styles/project.style.js
+++ src/styles/project.style.js
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
 export const StyledProject = styled.section`
-  padding: 0 var(--padding-body) var(--padding-body-sm);
+  padding-inline: var(--padding-body);
+  padding-block: var(--padding-body-sm);
   text-align: center;
   h2 {
     text-align: start;
     color: var(--purple);
     font-size: var(--fs-sm);
     padding-top: var(--padding-subject);
+  }
+
+  div.more {
+    color: var(--white);
+    width: fit-content;
+    margin: 40px auto 0;
+    padding: 5px;
+    cursor: pointer;
+    border-bottom: 3px solid var(--purple);
+    &:hover {
+      color: var(--gray);
+      border-bottom: 3px solid var(--gray);
+    }
   }
 `;
