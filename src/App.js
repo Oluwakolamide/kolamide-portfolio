0736f82b58fbe7d784bd611747b2ac4ad0ebe469
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-02-06 00:16:05 +0100

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-//import Socials from "./components/sections/socials";
-//import Title from "./components/sections/title";
-//import Technologies from "./components/sections/technologies";
-//import Button from "./components/sections/button";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import About from "./components/about";
@@ -12,7 +8,6 @@ import { StyledApp } from "./styles/app.style";
 import GlobalStyle from "./styles/GlobalStyle";
 import Footer from "./components/footer";
 import Experience from "./components/experience";
-//import ProjectCard from "./components/sections/projectcard";
 import Projects from "./components/projects";
 
 function App() {
@@ -20,10 +15,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <StyledApp>
-        {/* <Socials/> */}
-        {/* <Title sn={"01"} title={"Meet me"}/> */}
-        {/* <Technologies tech={"HTML"}/> */}
-        {/* <Button text={"Download my resume"}/> */}
         <Navbar />
         <Hero />
         <About />
