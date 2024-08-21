c4c3e723354b18727507ee3ed202576f4e15aa8a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-08-21 07:23:10 +0100

    updated skills

--- src/static/skills.js
+++ src/static/skills.js
@@ -1,26 +1,33 @@
 import {
-  Globe,
-  Browser,
-  AppleLogo,
-  AndroidLogo,
-  FigmaLogo,
   AppWindow,
+  Database,
+  HardDrives,
+  ChartBar,
+  ChartPieSlice,
+  Network,
+  Cloud,
+  Desktop,
 } from "@phosphor-icons/react";
 
 export const skills = [
   {
     id: 1,
-    name: "Web Development",
-    icon: [<Globe size={24} />, <Browser size={24} />],
-  },
-  {
-    id: 2,
-    name: "Mobile App Development",
-    icon: [<AppleLogo size={24} />, <AndroidLogo size={24} />],
+    name: "Data Analysis/Science",
+    icon: [<ChartBar size={24} />, <ChartPieSlice size={24} />],
   },
   {
     id: 3,
-    name: "UI/UX Design",
-    icon: [<FigmaLogo size={24} />, <AppWindow size={24} />],
+    name: "Database Administration",
+    icon: [<Database size={24} />, <HardDrives size={24} />],
+  },
+  {
+    id: 3,
+    name: "Network Security",
+    icon: [<Network size={24} />, <AppWindow size={24} />],
+  },
+  {
+    id: 3,
+    name: "Cloud Computing",
+    icon: [<Cloud size={24} />, <Desktop size={24} />],
   },
 ];
